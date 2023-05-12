f0f19dee511712056eb190a24f0623822d5a4520
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2023-05-12 09:12:13 +0200

    modif authentification

--- test.py
+++ test.py
@@ -10,24 +10,23 @@ driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
 def authentification_netflix():
-    driver.get("https://www.netflix.com/fr-en/login")   #accès à netflix
+    
+    driver.get("https://www.netflix.com/fr-en/login")   #accès à la page de connexion netflix
     
     #A changer selon le compte utilisé
-    username = "mail"
-    password = "mot_de_passe"
-    
-    #On enleve la page qui demande d'accepter les cookies
-    refuse_button = wait.until(EC.element_to_be_clickable((By.ID, "cookie-disclosure-reject")))
-    refuse_button.click()
+    username = "usename"
+    password = "mdp"
 
     #authentification
-    username_input = wait.until(EC.visibility_of_element_located((By.ID, "email")))   #le "email" faut aller le chercher dans le coee HTML de la page et c'est ce qu'il y a derrière name=
-    username_input.send_keys(username)
-    username_input.submit()
-
-    password_input = wait.until(EC.visibility_of_element_located((By.ID, "password")))  #pareil pour le password mais j'ai pas encore eu le temps de fairen
-    password_input.send_keys(password)
-    password_input.submit()
+    username_input = wait.until(EC.visibility_of_element_located((By.ID, "id_userLoginId")))   #cherche à partir de l'id l'encart pour l'identifiant
+    username_input.send_keys(username)  #envoie l'identifiant
+    username_input = wait.until(EC.visibility_of_element_located((By.ID, "id_password")))   #cherche à partir de l'id l'encart pour le mot de passe
+    username_input.send_keys(password) #envoie le mot de passe
+    driver.find_element(By.CSS_SELECTOR, 'button[data-uia="login-submit-button"]').click() #clique sur le bouton de connexion
+    profil_selection = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[data-uia="action-select-profile+secondary"]')))
+    profil_selection.click()
+    time.sleep(500)
+    driver.quit() #ferme le navigateur
 
 
 
