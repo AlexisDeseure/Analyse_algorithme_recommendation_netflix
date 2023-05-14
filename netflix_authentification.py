dd6d3e5a9716cd83f4d01a6ceece105e46f3541e
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2023-05-14 23:27:54 +0200

    modif authentification2

--- netflix_authentification.py
+++ netflix_authentification.py
@@ -9,30 +9,36 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 
+
 def authentification_netflix():
-    
-    driver.get("https://www.netflix.com/fr-en/login")   #accès à la page de connexion netflix
-    
-    #A changer selon le compte utilisé
-    username = "usename"
-    password = "mdp"
+    driver.get("https://www.netflix.com/fr-en/login")  # accès à netflix
 
-    #authentification
-    username_input = wait.until(EC.visibility_of_element_located((By.ID, "id_userLoginId")))   #cherche à partir de l'id l'encart pour l'identifiant
-    username_input.send_keys(username)  #envoie l'identifiant
-    username_input = wait.until(EC.visibility_of_element_located((By.ID, "id_password")))   #cherche à partir de l'id l'encart pour le mot de passe
-    username_input.send_keys(password) #envoie le mot de passe
-    driver.find_element(By.CSS_SELECTOR, 'button[data-uia="login-submit-button"]').click() #clique sur le bouton de connexion
-    profil_selection = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[data-uia="action-select-profile+secondary"]')))
-    profil_selection.click()
-    time.sleep(500)
-    driver.quit() #ferme le navigateur
+    # A changer selon le compte utilisé
+    username = "celpat@free.fr"
+    password = "zk98eg!+"
 
+    '''#On enleve la page qui demande d'accepter les cookies
+    refuse_button = wait.until(EC.element_to_be_clickable((By.ID, "cookie-disclosure-reject")))
+    refuse_button.click()'''
+
+    # authentification
+    username_input = wait.until(EC.visibility_of_element_located((By.ID,
+                                                                  "id_userLoginId")))  # le "email" faut aller le chercher dans le coee HTML de la page et c'est ce qu'il y a derrière name=
+    username_input.send_keys(username)
+
+    password_input = wait.until(EC.visibility_of_element_located(
+        (By.ID, "id_password"))) 
+    password_input.send_keys(password) 
+    password_input.submit()
+    element = wait.until(EC.visibility_of_element_located(
+        (By.XPATH, "//a[@href='/SwitchProfile?tkn=ZC5VAV2RCFEEBESIO2SWJWDUN4']"))) 
+    element.click()
+
+    time.sleep(10)
 
 
 def main():
     authentification_netflix()
-    print("Hello world")
 
 
 if __name__ == "__main__":
