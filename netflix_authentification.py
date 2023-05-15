4f0788e4d9630fcfa4a95b5008553cd4675000ea
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2023-05-15 22:14:56 +0200

    récupération noms films par ligne

--- netflix_authentification.py
+++ netflix_authentification.py
@@ -1,24 +1,22 @@
 import time
-import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import pandas as pd
+from dotenv import load_dotenv #permet de définir des variables d'environnement pour cacher les identifiants
+import os
 
 
 
-def authentification_netflix(driver, wait):
+def authentification_netflix():
     driver.get("https://www.netflix.com/fr-en/login")  # accès à netflix
 
     # A changer selon le compte utilisé
-    username = "usr"
-    password = "mdp"
-
-    '''#On enleve la page qui demande d'accepter les cookies
-    refuse_button = wait.until(EC.element_to_be_clickable((By.ID, "cookie-disclosure-reject")))
-    refuse_button.click()'''
+    load_dotenv('variables_de_connexion.env')
+    username = os.getenv('IDENTIFIANT')
+    password = os.getenv('MOT_DE_PASSE')
 
     # authentification
     username_input = wait.until(EC.visibility_of_element_located((By.ID,
@@ -36,35 +34,71 @@ def authentification_netflix(driver, wait):
 
     
 
-def recuperer_liste_ligne(driver,wait, ligne):
+def recuperer_liste_ligne():
     '''récupérer la liste des noms et liens des séries/films situés sur la ligne donnée en paramètre'''
-    wait.until(EC.presence_of_element_located((By.ID, ligne)))
-    #récupération du code html de la ligne
-    html = driver.find_element(By.ID,'row-1').get_attribute('innerHTML')
+    wait.until(EC.presence_of_element_located((By.ID, 'main-view')))
+    time.sleep(10)
+    #récupération du code html
+    html = driver.find_element(By.ID,'main-view').get_attribute('innerHTML')
     #création d'un objet BeautifulSoup pour pouvoir parser le code html
     soup = BeautifulSoup(html, 'html.parser')
-    #récupération des balises <a> qui contiennent les liens et les noms des séries/films (class="slider-refocus")
-    a_tags = soup.find_all('a', {'class': 'slider-refocus'})
+    #récupération des balises <a> qui contiennent les liens et les noms des séries/films (class="rowTitle ltr-0")
+    a_tags = soup.find_all('a', {'class': 'rowTitle ltr-0'})
+    # divs = soup.find_all('div', {'class': 'row-header-title'})
+    divs = soup.select('a > div.row-header-title')
+    textes = [div.text for div in divs]
     data = []
-    for a in a_tags:
+    # print(f"\n\n\n\n{a_tags}")
+    # print(textes)
+    for a,t in zip(a_tags, textes):
         href = a.get('href')
-        title = a.get('aria-label')
-        data.append([href, title])
-    df = pd.DataFrame(data, columns=['liens', 'titres'])
+        data.append([href, t])
+    df = pd.DataFrame(data, columns=['liens', 'titres_catégories'])
     # Stockage du DataFrame dans un fichier CSV
     df.to_csv('data.csv', index=False)
 
-
-
+def recuperer_titres_catégorie(ligne,directory_name):
+    driver.get(f"https://www.netflix.com{ligne[1]}")
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'ltr-1pq5s1g')))
+    time.sleep(5)
+    html = driver.find_element(By.CLASS_NAME,'ltr-1pq5s1g').get_attribute('innerHTML')
+    soup = BeautifulSoup(html, 'html.parser')
+    a_tags = soup.find_all('a', {'class': 'slider-refocus'})
+    img_tags = soup.find_all('img', {'class': 'boxart-image boxart-image-in-padded-container'})
+    ps = soup.find_all('p', {'class': 'fallback-text'})
+    textes = [p.text for p in ps]
+    data = []
+    for a,img,t in zip(a_tags,img_tags,textes):
+        href = a.get('href')
+        src = img.get('src')
+        data.append([t,href,src])
+    df = pd.DataFrame(data, columns=['titres', 'liens', 'liens_images'])
+    df.to_csv(f'./{directory_name}/{ligne[2]}.csv', index=False)
+    
+def recuperer_tous_titres():
+    directory_name = "listes_csv"
+    if not os.path.exists(directory_name):
+        # si le répertoire n'existe pas, créez-le
+        os.mkdir(directory_name)
+    df = pd.read_csv('data.csv')
+    for lignes in df.itertuples():
+        print(lignes[2]+" en cours de récupération")
+        recuperer_titres_catégorie(lignes,directory_name)
+        print(lignes[2]+" récupéré")
 
 def main():
-    # Configuration du navigateur
-    driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 10)
-    authentification_netflix(driver, wait)
-    recuperer_liste_ligne(driver, wait,"row-1")
-    time.sleep(1000)
+    authentification_netflix()
+    recuperer_liste_ligne()
+    recuperer_tous_titres()
+    time.sleep(10)
 
 
 if __name__ == "__main__":
+    # Configuration du navigateur
+    driver = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)
     main()
+    wait.quit()
+    driver.quit()
+    
+
