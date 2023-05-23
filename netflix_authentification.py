80e6c0d930fab90de538713154c9f44333f2dcf1
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2023-05-23 22:43:09 +0200

    Modifications diverses

--- netflix_authentification.py
+++ netflix_authentification.py
@@ -14,8 +14,20 @@ import os
 
 
 
-def authentification_netflix():
+def authentification_netflix(headless=True):
     """fonction permettant de s'authentifier sur netflix"""
+    # Configuration de Chrome
+    chrome_options = Options()
+    if (headless):
+        chrome_options.add_argument("--headless")  # Exécuter Chrome en mode headless
+    chrome_options.add_argument("--silent") # Exécuter Chrome en mode silencieux
+    chrome_options.add_argument("--disable-logging")  # Désactiver les messages de la console
+    chrome_options.add_argument("--log-level=3")  # Définir le niveau de journalisation de la console
+    chrome_options.add_argument("--mute-audio") # Désactiver le son
+
+    # Création de l'instance de Chrome avec les options
+    driver = webdriver.Chrome(options=chrome_options)
+    wait = WebDriverWait(driver, 10)
     driver.get("https://www.netflix.com/fr-en/login")  # accès à netflix
 
     # A changer selon le compte utilisé
@@ -35,11 +47,13 @@ def authentification_netflix():
     element = wait.until(EC.visibility_of_element_located(
         (By.XPATH, f"//a[@href='/SwitchProfile?tkn={os.getenv('TOKEN')}']")))
     element.click()
+    return driver
 
     
 
-def recuperer_liste_ligne():
+def recuperer_liste_ligne(driver):
     '''récupérer la liste des noms et liens des séries/films situés sur la ligne donnée en paramètre'''
+    wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_element_located((By.ID, 'main-view')))
     # Appuyer sur la touche bas dans la fenêtre active
     for _ in range(300):
@@ -62,10 +76,11 @@ def recuperer_liste_ligne():
         data.append([f"https://www.netflix.com{href}", t])
     df = pd.DataFrame(data, columns=['liens', 'titres_catégories'])
     # Stockage du DataFrame dans un fichier CSV
-    df.to_csv('categories.csv', index=False)
+    df.to_csv('categories.csv', index=False,sep=';')
 
-def recuperer_titres_catégorie(ligne,directory_name):
+def recuperer_titres_catégorie(driver,ligne,directory_name):
     """récupérer les titres de la catégorie située sur la ligne donnée en paramètre"""
+    wait = WebDriverWait(driver, 10)
     driver.get(ligne[1])
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'ltr-1pq5s1g')))
     time.sleep(5)
@@ -81,30 +96,43 @@ def recuperer_titres_catégorie(ligne,directory_name):
         src = img.get('src')
         data.append([t,href.split('?')[0][7:],src]) #href.split('?')[0][7:] permet de récupérer l'ID du film/série à partir du href de la balise <a>
     df = pd.DataFrame(data, columns=['titres', 'ID', 'liens_images'])
-    df.to_csv(f'./{directory_name}/{ligne[2]}.csv', index=False)
+    df.to_csv(f'./{directory_name}/{ligne[2]}.csv', index=False,sep=';')
     
-def recuperer_tous_titres():
+def recuperer_tous_titres(driver):
     '''récupérer les titres de toutes les catégories'''
     directory_name = "listes_csv"
     if not os.path.exists(directory_name):
         # si le répertoire n'existe pas, créez-le
         os.mkdir(directory_name)
-    df = pd.read_csv('categories.csv')
+    df = pd.read_csv('categories.csv',sep=';')
     i = 0
     longueur = len(df)
     for lignes in df.itertuples():
         i+=1
         print(f"{i}/{longueur} : {lignes[2]} en cours de récupération...", end='', flush=True)
-        recuperer_titres_catégorie(lignes,directory_name)
+        recuperer_titres_catégorie(driver,lignes,directory_name)
         print(f"\r{i}/{longueur} : {lignes[2]} récupéré"+" "*50)
 
-def parcourt_csv(file,nom_categorie, like=None):
-    global driver, wait
+def relancer_driver(driver):
+    '''relance le driver'''
+    driver.quit()
+    print("\nLa présence de l'élément n'a pas pu être vérifiée dans le délai imparti.")
+    #création d'un nouveau driver pour contourner le ban de Netflix
+    driver = authentification_netflix()
+    time.sleep(3)
+    return driver
+
+def parcourt_csv(driver,file,nom_categorie,longueur_totale,index_total,like=None):
     '''parcourt le fichier csv donné en paramètre et récupère les informations de chaque film/série'''
-    df = pd.read_csv(file)
+    wait = WebDriverWait(driver, 10)
+    df = pd.read_csv(file, sep=';')
     data = []
     time.sleep(1) #important pour que la requete fonctionne
+    longueur = len(df)
+    i=0
     for lignes in df.itertuples():
+        i+=1
+        print(f"\r{index_total}/{longueur_totale} : \"{nom_categorie}\" en cours de traitement... {i}/{longueur}"+" "*50, end='', flush=True)
         driver.get(f"https://www.netflix.com/title/{lignes.ID}")
         time.sleep(1) #important pour l'affichage
         if like in ["like", "dislike", "love"]:
@@ -146,12 +174,8 @@ def parcourt_csv(file,nom_categorie, like=None):
             span_prevent = soup.find('span', {'class': 'ltr-1q4vxyr'})
             div_mise_en_avant_supp = soup.find('div', {'class': 'supplemental-message'})
         except TimeoutException:
-            print("\nLa présence de l'élément n'a pas pu être vérifiée dans le délai imparti.")
-            driver = webdriver.Chrome()
+            driver = relancer_driver(driver)
             wait = WebDriverWait(driver, 10)
-            #création d'un nouveau driver et d'un nouveau wait pour contourner le ban de Netflix
-            authentification_netflix()
-            time.sleep(3)
         if div_mise_en_avant_supp is not None:
             div_mise_en_avant_supp = True
         else:
@@ -180,12 +204,8 @@ def parcourt_csv(file,nom_categorie, like=None):
                             else:
                                 tab_a_propos[dic_a_propos[key]] += f",{valeur.text.strip().replace(',', '')}"
         except TimeoutException:
-            print("\nLa présence de l'élément n'a pas pu être vérifiée dans le délai imparti.")
-            driver = webdriver.Chrome()
+            driver = relancer_driver(driver)
             wait = WebDriverWait(driver, 10)
-            #création d'un nouveau driver et d'un nouveau wait pour contourner le ban de Netflix
-            authentification_netflix()
-            time.sleep(3)
         try:
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'moreLikeThis--container')))
             html = driver.find_element(By.CLASS_NAME, 'moreLikeThis--container')
@@ -201,12 +221,8 @@ def parcourt_csv(file,nom_categorie, like=None):
             recommendations = recommendations[:-1] #on enlève la dernière virgule
         except:
             recommendations = None
-            driver.quit()
-            driver = webdriver.Chrome()
+            driver = relancer_driver(driver)
             wait = WebDriverWait(driver, 10)
-            #création d'un nouveau driver et d'un nouveau wait pour contourner le ban de Netflix
-            authentification_netflix()
-            time.sleep(3)
         data.append([nom_categorie,
                      lignes.titres,
                      lignes.ID,
@@ -241,31 +257,31 @@ def parcourt_csv(file,nom_categorie, like=None):
                                      'genres', 
                                      'avertissement_programme',
                                      'recommendations'])
-    return df
+    print(f"\r{index_total}/{longueur_totale} : \"{nom_categorie}\"  traité"+" "*50)
+    return df, driver
 
 
-def parcourt_titres_informations():
+def parcourt_titres_informations(driver, like=None):
     """parcourt les fichiers csv contenant les titres et récupère les informations de chaque titre"""
     directory = "listes_csv"
     longueur = len(os.listdir(directory))
-    i=0
     first = True
+    i=0
     for file in os.listdir(directory):
         i+=1
-        print(f"{i}/{longueur} : {file} en cours de traitement...", end='', flush=True)
         file_path = os.path.join(directory, file)
         # Vérifier si le chemin correspond à un fichier csv et ne pas inclure les sous-dossiers
         if os.path.isfile(file_path) and file.endswith('.csv'):
-            action = None #à modifier pour que le bot fasse une action de like/dislike sur chaque titre
             if first:
-                df = parcourt_csv(file_path,file[:-4], like=action)
-                df.to_csv('bdd_series.csv', index=False)
+                df,driver = parcourt_csv(driver, file_path,file[:-4],longueur,i,like)
+                df.to_csv('bdd_series.csv', index=False, sep=';')
                 first = False
             else:
-                df = pd.read_csv('bdd_series.csv')
-                df = pd.concat([df, parcourt_csv(file_path,file[:-4], like=action)],axis=0)
-                df.to_csv('bdd_series.csv', index=False)
-        print(f"\r{i}/{longueur} : {file}  traité"+" "*50)
+                df = pd.read_csv('bdd_series.csv',sep=';')
+                df_inter, driver = parcourt_csv(driver, file_path,file[:-4],longueur,i,like)
+                df = pd.concat([df, df_inter],axis=0)
+                df.to_csv('bdd_series.csv', index=False, sep=';')
+    return driver
     
 def get_first_non_null(values):
     for value in values:
@@ -281,7 +297,7 @@ def process_multiple_values(values, aggregation_type=False):
     for value in values:
         if value != 'nan':
             unique_values.update(value.split(',') if aggregation_type else [value])
-    return (',' if aggregation_type else ';').join(str(v) for v in unique_values) if unique_values!=set() else None
+    return (',' if aggregation_type else '|').join(str(v) for v in unique_values) if unique_values!=set() else None
 
 def nombre_mise_en_avant(values):
     if all(value == 'nan' for value in values):
@@ -291,7 +307,7 @@ def nombre_mise_en_avant(values):
 def gestion_doublons(file_path):
     """supprime les doublons dans un fichier csv"""
     # Charger le fichier CSV dans un DataFrame pandas
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path,sep=';')
     df = df.astype(str)
     # Regrouper les données par l'ID et appliquer les opérations d'agrégation sur les colonnes pertinentes
     grouped_df = df.groupby('ID').agg({
@@ -313,58 +329,64 @@ def gestion_doublons(file_path):
         'recommendations': lambda x: process_multiple_values(x,True)
     }).reset_index()
     grouped_df['nombre_occurrence'] = df.groupby('ID').size().reset_index(name='count')['count']
-    grouped_df.to_csv(file_path[:-4] + '_modifie.csv', index=False)
+    grouped_df.to_csv(file_path[:-4] + '_modifie.csv', index=False, sep=';')
 
 def archiver_csv():
-    """archive les fichiers csv contenant les titres dans un dossier"""
+    """Archive les fichiers CSV contenant les titres dans des dossiers"""
     directory_name = "listes_archivees"
     if not os.path.exists(directory_name):
-        # si le répertoire n'existe pas, créez-le
+        # Si le répertoire n'existe pas, créez-le
         os.mkdir(directory_name)
+
+    # Recherche du dernier numéro d'itération
+    last_iteration = 0
+    for folder in os.listdir(directory_name):
+        if folder.startswith("itération"):
+            iteration_number = int(folder.split(" ")[1])
+            last_iteration = max(last_iteration, iteration_number)
+
+    # Création du nouveau dossier d'itération
+    new_iteration = last_iteration + 1
+    new_iteration_folder = f"itération {new_iteration}"
+    new_iteration_path = os.path.join(directory_name, new_iteration_folder)
+    os.mkdir(new_iteration_path)
+
     for file in os.listdir("listes_csv"):
         file_path = os.path.join("listes_csv", file)
-        # Vérifier si le chemin correspond à un fichier csv et ne pas inclure les sous-dossiers
+        # Vérifier si le chemin correspond à un fichier CSV et ne pas inclure les sous-dossiers
         if os.path.isfile(file_path) and file.endswith('.csv'):
-            os.rename(file_path, os.path.join(directory_name, file))
+            # Copie du fichier dans le nouveau dossier d'itération
+            new_file_path = os.path.join(new_iteration_path, file)
+            os.rename(file_path, new_file_path)
 
 def main():
     """fonction principale"""
     print("Authentification en cours...")
-    authentification_netflix()
+    driver = authentification_netflix()
     print("Authentification réussie\n")
     print("Récupération des catégories en cours...")
-    recuperer_liste_ligne()
+    recuperer_liste_ligne(driver)
     print("Catégories récupérées\n")
     print("Récupération des titres en cours...")
-    recuperer_tous_titres()
+    recuperer_tous_titres(driver)
     print("Titres récupérés\n")
     print("Récupération des informations en cours...")
-    parcourt_titres_informations() 
+    driver = parcourt_titres_informations(driver, like=None) 
     print("Informations récupérées\n")
+    print("Fermeture du navigateur...")
+    driver.quit()
+    print("Navigateur fermé\n")
     print("Archivage en cours...")
     archiver_csv()
     print("Archivage terminé\n")
     print("Gestion des doublons en cours...")
     gestion_doublons('bdd_series.csv')
     print("Doublons gérés\n")
-    print("Fermeture du navigateur...")
-
-if __name__ == "__main__":
-
-    # Configuration de Chrome
-    chrome_options = Options()
-    # ''' à commenter pour ne plus afficher la fenêtre chrome 
-    chrome_options.add_argument("--headless")  # Exécuter Chrome en mode headless
-    #  '''
-    chrome_options.add_argument("--silent") # Exécuter Chrome en mode silencieux
-    chrome_options.add_argument("--disable-logging")  # Désactiver les messages de la console
-    chrome_options.add_argument("--log-level=3")  # Définir le niveau de journalisation de la console
+    print("Fin du programme")
     
 
-    # Création de l'instance de Chrome avec les options
-    driver = webdriver.Chrome(options=chrome_options)
-    wait = WebDriverWait(driver, 10)
+if __name__ == "__main__":
     main()
-    driver.quit()
+    
     
 
