689280b89b044488c4e0532216c6eb10fb331b6a
ab-tf <122019345+ab-tf@users.noreply.github.com>
2023-06-07 00:21:17 +0200

    Add files via upload

---
+++ fonctions_csv.py
@@ -0,0 +1,51 @@
+import csv
+
+# Chemin du fichier CSV
+csv_file_path = 'bdd_series_modifie.csv'
+
+def modifier_csv(csv_file_path):
+    # Chemin du nouveau fichier CSV
+    new_csv_file_path = 'nouveau_fichier.csv'
+    
+    # Colonnes à conserver
+    columns_to_keep = ['ID', 'recommendations']
+    
+    # Ouvrir le fichier d'entrée en lecture
+    with open(csv_file_path, 'r', encoding='utf-8') as input_file:
+        reader = csv.DictReader(input_file, delimiter=';')
+        fieldnames = reader.fieldnames
+        
+        # Créer une liste de dictionnaires pour stocker les lignes à conserver avec les colonnes étendues
+        rows_extended = []
+        
+        # Obtenir le nombre maximum de recommandations
+        max_recommendations = 0
+        
+        # Parcourir chaque ligne du fichier d'entrée
+        for row in reader:
+            # Supprimer les colonnes inutiles et conserver les colonnes spécifiées
+            new_row = {column: row[column] for column in columns_to_keep}
+            
+            # Étendre les recommandations en colonnes individuelles
+            recommendations = row["recommendations"].split(",")
+            max_recommendations = max(max_recommendations, len(recommendations))
+            for i, recommendation in enumerate(recommendations, start=1):
+                column_name = f"recommendation{i}"
+                new_row[column_name] = recommendation
+            
+            rows_extended.append(new_row)
+    
+    # Obtenir la liste des noms de colonnes étendues
+    extended_fieldnames = columns_to_keep + [f"recommendation{i}" for i in range(1, max_recommendations + 1)]
+    
+    # Écrire les lignes avec les colonnes étendues dans un nouveau fichier CSV de sortie
+    with open(new_csv_file_path, 'w', newline='', encoding='utf-8') as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=extended_fieldnames, delimiter=';')
+        writer.writeheader()
+        writer.writerows(rows_extended)
+    
+    print("Les colonnes ont été supprimées et les recommandations ont été triées avec succès.")
+    print("Le fichier de sortie est prêt :", new_csv_file_path)
+    print("Les colonnes du fichier de sortie :", extended_fieldnames)
+
+modifier_csv(csv_file_path)
